91721ee6590a7ef21d3161c08f21a7c2de6ab86f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-04 20:07:45 +0000

    refactor spinner2.js to only use 8 lines

--- spinner2.js
+++ spinner2.js
@@ -1,35 +1,8 @@
-setTimeout(() => {
-  process.stdout.write("\r|   ");
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write("\r-   ");
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write("\r\\   ");
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write("\r|  ");
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write("\r/   ");
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write("\r-   ");
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write("\r\\   ");
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write("\r|  ");
-}, 1700);
+const array = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|"];
+ms = 100;
+for (const char of array) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}   `);
+  }, ms);
+  ms += 200;
+}
